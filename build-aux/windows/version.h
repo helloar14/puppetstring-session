5eeeeb53c1c6a967e77755d3384cc24a0f911976
AR14 Works <hello@ar14.works>
2024-06-22 04:34:06 +0700

    Update version.h

--- build-aux/windows/version.h
+++ build-aux/windows/version.h
@@ -7,13 +7,13 @@
 #define VER_PRODUCTVERSION          0,8,1,0
 #define VER_PRODUCTVERSION_STR      "0.8.1\0"
 
-#define VER_COMPANYNAME_STR         "Inochi2D Project"
-#define VER_FILEDESCRIPTION_STR     "Inochi Session"
-#define VER_INTERNALNAME_STR        "inochi-session"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2022-2023 Inochi2D Project"
-#define VER_ORIGINALFILENAME_STR    "inochi-session.exe"
-#define VER_PRODUCTNAME_STR         "Inochi Session"
+#define VER_COMPANYNAME_STR         "AR14.works"
+#define VER_FILEDESCRIPTION_STR     "Puppetstring Viewer"
+#define VER_INTERNALNAME_STR        "puppetstring-viewer"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2024 AR14.works"
+#define VER_ORIGINALFILENAME_STR    "Viewer.exe"
+#define VER_PRODUCTNAME_STR         "Puppetstring Viewer"
 
-#define VER_COMPANYDOMAIN_STR       "inochi2d.com"
+#define VER_COMPANYDOMAIN_STR       "AR14.works"
 
 #endif // VERSION_H
